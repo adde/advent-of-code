9bcb3f8ea90aa53f6108b80b2c6e7b6f632675a6
Andreas Jönsson <adde90@gmail.com>
2024-01-02 21:54:45 +0100

    Optimize code for part two
    Adapt the function for part one to search in reverse instead of looping all possible start pos. This is ~40 x faster.

--- 2022/12/main.go
+++ 2022/12/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"container/heap"
 	"fmt"
-	"math"
 	"time"
 
 	"github.com/adde/advent-of-code/utils"
@@ -43,10 +42,10 @@ func main() {
 
 	// Populate the grid from input
 	grid := parseInput(utils.ReadLines("input.txt"))
-	start := getPos('S', grid)
+	start, end := getPos('S', grid), getPos('E', grid)
 
-	fmt.Println("\nFewest steps to target(p1):", getShortestDistance(grid, start.Row, start.Col))
-	fmt.Println("Fewest steps to target(p2):", getShortestDistanceAll(grid))
+	fmt.Println("\nFewest steps to target(p1):", getShortestDistance(grid, start.Row, start.Col, false))
+	fmt.Println("Fewest steps to target(p2):", getShortestDistance(grid, end.Row, end.Col, true))
 	fmt.Printf("Elapsed time: %s\n\n", time.Since(startTime))
 }
 
@@ -67,7 +66,7 @@ func parseInput(lines []string) [][]rune {
 	return grid
 }
 
-func getShortestDistance(grid [][]rune, startR, startC int) int {
+func getShortestDistance(grid [][]rune, startR, startC int, reverse bool) int {
 	seen := make(map[string]int)
 	rowLen, colLen := len(grid), len(grid[0])
 
@@ -80,7 +79,8 @@ func getShortestDistance(grid [][]rune, startR, startC int) int {
 		cell := heap.Pop(pq).(Cell)
 
 		// If we reached the target, just return the distance
-		if cell.Row == target.Row && cell.Col == target.Col {
+		if (!reverse && cell.Row == target.Row && cell.Col == target.Col) ||
+			(reverse && grid[cell.Row][cell.Col] == 'a') {
 			return cell.Dist
 		}
 
@@ -97,29 +97,13 @@ func getShortestDistance(grid [][]rune, startR, startC int) int {
 
 			// If the next cell is a valid move, add it to the priority queue
 			if r >= 0 && r < rowLen && c >= 0 && c < colLen &&
-				isValidMove(grid, cell.Row, cell.Col, r, c) {
+				isValidMove(grid, cell.Row, cell.Col, r, c, reverse) {
 				heap.Push(pq, Cell{r, c, cell.Dist + 1})
 			}
 		}
 	}
 
 	return 0
-}
-
-func getShortestDistanceAll(grid [][]rune) int {
-	startPositions := getPossibleStartPos('a', grid)
-
-	// Get the shortest distance for each start position
-	shortest := math.MaxInt32
-	for _, pos := range startPositions {
-		distance := getShortestDistance(grid, pos.Row, pos.Col)
-
-		if distance != 0 {
-			shortest = min(shortest, distance)
-		}
-	}
-
-	return shortest
 }
 
 func getPos(char rune, grid [][]rune) Cell {
@@ -137,21 +121,7 @@ func getPos(char rune, grid [][]rune) Cell {
 	return pos
 }
 
-func getPossibleStartPos(char rune, grid [][]rune) []Cell {
-	var positions []Cell
-
-	for r, row := range grid {
-		for c := range row {
-			if grid[r][c] == char || grid[r][c] == 'S' {
-				positions = append(positions, Cell{r, c, 0})
-			}
-		}
-	}
-
-	return positions
-}
-
-func isValidMove(grid [][]rune, row, col, newRow, newCol int) bool {
+func isValidMove(grid [][]rune, row, col, newRow, newCol int, reverse bool) bool {
 	a := grid[row][col]
 	b := grid[newRow][newCol]
 
@@ -168,5 +138,5 @@ func isValidMove(grid [][]rune, row, col, newRow, newCol int) bool {
 		b = 'z'
 	}
 
-	return b-a <= 1
+	return (!reverse && b-a <= 1) || (reverse && a-b <= 1)
 }
