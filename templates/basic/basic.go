797c5656699f61aad56ada132b2c2dbc375408b3
Andreas Jönsson <adde90@gmail.com>
2024-12-06 21:01:41 +0100

    Update basic template

--- templates/basic/basic.go
+++ templates/basic/basic.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/adde/advent-of-code/utils"
+	u "github.com/adde/advent-of-code/utils"
 )
 
 func main() {
 	startTime := time.Now()
-
-	lines := utils.ReadLines("input.txt")
+	lines := u.ReadLines("input.txt")
 
 	for _, line := range lines {
 		fmt.Println(line)
