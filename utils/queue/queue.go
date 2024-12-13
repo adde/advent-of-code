71f9116e6ce27e46763befabe1ff8942fc474663
Andreas Jönsson <adde90@gmail.com>
2024-12-13 19:11:08 +0100

    Add queue util type

---
+++ utils/queue/queue.go
@@ -0,0 +1,27 @@
+package queue
+
+type Queue[T comparable] []T
+
+func New[T comparable](item T) Queue[T] {
+	q := make(Queue[T], 0)
+	q.Append(item)
+	return q
+}
+
+func (q *Queue[T]) Append(item T) {
+	*q = append(*q, item)
+}
+
+func (q *Queue[T]) Pop() T {
+	item := (*q)[0]
+	*q = (*q)[1:]
+	return item
+}
+
+func (q Queue[T]) Size() int {
+	return len(q)
+}
+
+func (q Queue[T]) IsEmpty() bool {
+	return len(q) == 0
+}
