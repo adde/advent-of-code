be83de8562b676a2d2faea082f97481c863dc4ac
Andreas Jönsson <adde90@gmail.com>
2024-12-18 12:11:20 +0100

    Update dijkstras template to use util funcs

--- templates/dijkstras/dijkstras.go
+++ templates/dijkstras/dijkstras.go
@@ -1,108 +1,62 @@
 package main
 
 import (
-	"bufio"
 	"container/heap"
 	"fmt"
-	"log"
-	"os"
 	"time"
 
-	"github.com/adde/advent-of-code/utils"
+	u "github.com/adde/advent-of-code/utils"
+	g "github.com/adde/advent-of-code/utils/grid"
+	pq "github.com/adde/advent-of-code/utils/priorityqueue"
+	"github.com/adde/advent-of-code/utils/set"
 )
 
-type Cell struct {
-	Row, Col, Dist int
-}
-
-type PriorityQueue []Cell
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].Dist < pq[j].Dist
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	item := x.(Cell)
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	*pq = old[0 : n-1]
-	return item
-}
-
 func main() {
-	var grid [][]int
-
 	startTime := time.Now()
+	lines := u.ReadLines("input.txt")
+	grid := g.CreateFromLines(lines)
 
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	distance := getShortestDistance(grid, 0, 0, len(grid)-1, len(grid[0])-1)
 
-	scanner := bufio.NewScanner(file)
-
-	r := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		grid = append(grid, make([]int, len(line)))
-
-		for c, v := range line {
-			grid[r][c] = utils.ToInt(string(v))
-		}
-
-		r++
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("\nThe shortest distance to target:", getShortestDistance(grid, len(grid[0])-1, len(grid)-1))
-	fmt.Printf("Elapsed time: %s\n\n", time.Since(startTime))
+	fmt.Println("\nPart one:", distance)
+	fmt.Println("Part two:")
+	fmt.Printf("\nExecution time: %s\n\n", time.Since(startTime))
 }
 
-func getShortestDistance(grid [][]int, targetX, targetY int) int {
-	seen := make(map[string]int)
-	rowLen, colLen := len(grid), len(grid[0])
+// Get the shortest distance from the start cell to the target cell in 2D grid
+func getShortestDistance(grid g.Grid, startRow, startCol, targetRow, targetCol int) int {
+	seen := set.New[[2]int]()
 
-	// Start at the top left corner
-	pq := &PriorityQueue{{0, 0, grid[0][0]}}
-	heap.Init(pq)
+	// Create a priority queue and add the starting cell
+	prioQueue := &pq.PriorityQueue{pq.Cell{Row: startRow, Col: startCol, Cost: 0}}
+	heap.Init(prioQueue)
 
-	for pq.Len() > 0 {
-		cell := heap.Pop(pq).(Cell)
+	for prioQueue.Len() > 0 {
+		cell := heap.Pop(prioQueue).(pq.Cell)
 
-		// If we reached the target, just return the distance
-		if cell.Row == targetY && cell.Col == targetX {
-			return cell.Dist
+		// Check if we reached the target cell, return the cost
+		if cell.Row == targetRow && cell.Col == targetCol {
+			return cell.Cost
 		}
 
 		// Check if we already visited this cell
-		key := fmt.Sprintf("%d,%d", cell.Row, cell.Col)
-		if _, ok := seen[key]; ok {
+		key := [2]int{cell.Row, cell.Col}
+		if seen.Contains(key) {
 			continue
 		}
-		seen[key] = cell.Dist
+		seen.Add(key)
 
-		// Check all four directions
+		// Check all four directions(Up, Right, Down, Left)
 		for _, dir := range [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
-			r, c := cell.Row+dir[0], cell.Col+dir[1]
+			newRow, newCol := cell.Row+dir[0], cell.Col+dir[1]
+
+			// Check if the next cell is outside the grid
+			if !grid.IsInsideBounds(newRow, newCol) {
+				continue
+			}
 
 			// If the next cell is a valid move, add it to the priority queue
-			if r >= 0 && r < rowLen && c >= 0 && c < colLen {
-				heap.Push(pq, Cell{r, c, cell.Dist + grid[r][c]})
-			}
+			heap.Push(prioQueue, pq.Cell{Row: newRow, Col: newCol, Cost: cell.Cost + 1})
 		}
 	}
 
